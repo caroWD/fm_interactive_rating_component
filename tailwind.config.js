52e0a2b787b3cf13b54e19ff5e64dee31e1d713c
caroDesign <juego.fuego@gmail.com>
2025-01-17 17:27:27 -0500

    Update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Overpass"', 'sistem-ui', 'sans-serif'],
+    },
+    colors: {
+      'orange': 'hsl(25, 97%, 53%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'light-grey': 'hsl(217, 12%, 63%)',
+      'dark-blue': 'hsl(213, 19%, 18%)',
+      'very-dark-blue': 'hsl(216, 12%, 8%)',
+    },
+    extend: {
+      screens: {
+        '2xs': '375px',
+        'xs': '425px',
+      },
+    },
   },
   plugins: [],
 }
